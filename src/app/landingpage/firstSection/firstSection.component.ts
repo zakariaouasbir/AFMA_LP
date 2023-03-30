b83a93ea416a6e55c78e087cae7b8a5f907478af
zakaria <zakariaouasbir@gmail.com>
2023-03-30 23:37:28 +0000

    Fix Card's image height -- First Section

--- src/app/landingpage/firstSection/firstSection.component.ts
+++ src/app/landingpage/firstSection/firstSection.component.ts
@@ -3,7 +3,7 @@ import { transition, trigger, useAnimation } from '@angular/animations';
 import { fadeInRight } from 'ng-animate';
 
 @Component({
-  selector: 'app-firstSection',
+  selector: 'app-firstsection',
   templateUrl: './firstSection.component.html',
   styleUrls: ['./firstSection.component.scss'],
   animations: [
